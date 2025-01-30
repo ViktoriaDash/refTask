60b5ab6ca6985ff34f178fc941aa9d472bd097bb
Roman Karbivskyi <vt231_krp@student.ztu.edu.ua>
2025-01-30 12:36:14 +0200

    Revert "remove: unnecessary do while loop;"
    This reverts commit 407356a0c10deb8d200626ac41f94f90b1ecd2c3.

--- js3.js
+++ js3.js
@@ -1,15 +1,20 @@
 let block = document.querySelector('.block');
 let button = document.querySelector('.bt');
 
-button.addEventListener('mouseover', moveButtonRandomly);
 
-function moveButtonRandomly() {
+button.addEventListener('mouseover',() =>{
     const blockRect = block.getBoundingClientRect();
     const buttonRect = button.getBoundingClientRect();
 
-    const posX = Math.random() * (blockRect.width - buttonRect.width);
-    const posY = Math.random() * (blockRect.height - buttonRect.height);
+    let posX, posY;
 
+    do {
+        posX = Math.random() * (blockRect.width - buttonRect.width);
+        posY = Math.random() * (blockRect.height - buttonRect.height);
+    } while (
+        posX < 0 || posX > blockRect.width - buttonRect.width ||
+        posY < 0 || posY > blockRect.height - buttonRect.height
+        );
     button.style.left = `${posX}px`;
     button.style.top = `${posY}px`;
-}
+})
