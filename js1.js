cfd77d6e93875c68d85ca12256470b0a71207886
Вікторія Коржовська <ipz234_kvv@student.ztu.edu.ua>
2025-01-30 15:26:03 +0200

    Refactored divergent changes in js1.js

--- js1.js
+++ js1.js
@@ -1,8 +1,8 @@
-let block = document.getElementById('block')
-let  sq = document.querySelectorAll('.sq')
+let block = document.getElementById('block');
+let sq = document.querySelectorAll('.sq');
 
-sq.forEach(square =>{
-    square.onmousedown = function(event){
+sq.forEach(square => {
+    square.onmousedown = function(event) {
         let shiftX = event.clientX - square.getBoundingClientRect().left;
         let shiftY = event.clientY - square.getBoundingClientRect().top;
 
@@ -11,28 +11,21 @@ sq.forEach(square =>{
 
         moveAt(event.pageX, event.pageY);
 
-        function moveAt(pageX, pageY){
+        function moveAt(pageX, pageY) {
             let blockRect = block.getBoundingClientRect();
             let squareRect = square.getBoundingClientRect();
 
-            let posX = pageX - shiftX;
-            if(posX < blockRect.left)
-                posX = blockRect.left;
-            if(posX + squareRect.width > blockRect.right)
-                posX = blockRect.right - squareRect.width;
-
-            let posY = pageY - shiftY;
-            if (posY < blockRect.top)
-                posY = blockRect.top;
-            if (posY + squareRect.height > blockRect.bottom)
-                posY = blockRect.bottom - squareRect.height;
+            let posX = getValidPosition(pageX - shiftX, blockRect.left, blockRect.right - squareRect.width);
+            let posY = getValidPosition(pageY - shiftY, blockRect.top, blockRect.bottom - squareRect.height);
 
             square.style.left = posX - blockRect.left + 'px';
             square.style.top = posY - blockRect.top + 'px';
         }
+
         function onMouseMove(event) {
             moveAt(event.pageX, event.pageY);
         }
+
         document.addEventListener('mousemove', onMouseMove);
 
         square.onmouseup = function() {
@@ -40,8 +33,14 @@ sq.forEach(square =>{
             square.onmouseup = null;
         };
     };
+
     square.ondragstart = function() {
         return false;
     };
-
 });
+
+function getValidPosition(position, min, max) {
+    if (position < min) return min;
+    if (position > max) return max;
+    return position;
+}
